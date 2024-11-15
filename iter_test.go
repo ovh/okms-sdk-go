2d9568b46b90870a22215a7c91b53f94a18f8211
Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>
2024-11-15 10:36:26 +0100

    test: unit tests for the key iterator
    Signed-off-by: Pierre-Henri Symoneaux <pierre-henri.symoneaux@ovhcloud.com>

---
+++ iter_test.go
@@ -0,0 +1,76 @@
+package okms
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ovh/okms-sdk-go/mocks"
+	"github.com/ovh/okms-sdk-go/types"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKeyIter(t *testing.T) {
+	mc := mocks.NewAPIMock(t)
+	client := Client{mc}
+
+	it := client.ListAllServiceKeys(nil, nil)
+
+	contToken := "abcdef"
+	keys := []types.GetServiceKeyResponse{
+		{Name: "a"},
+		{Name: "b"},
+		{Name: "c"},
+		{Name: "d"},
+	}
+
+	mc.EXPECT().ListServiceKeys(mock.Anything, (*string)(nil), (*int32)(nil), (*types.KeyStates)(nil)).
+		Return(&types.ListServiceKeysResponse{IsTruncated: true, ContinuationToken: &contToken, ObjectsList: keys[:2]}, nil).
+		Once()
+
+	mc.EXPECT().ListServiceKeys(mock.Anything, &contToken, (*int32)(nil), (*types.KeyStates)(nil)).
+		Return(&types.ListServiceKeysResponse{IsTruncated: false, ContinuationToken: nil, ObjectsList: keys[2:]}, nil).
+		Once()
+
+	i := 0
+	for k, err := range it.Iter(context.Background()) {
+		require.NoError(t, err)
+		require.EqualValues(t, &keys[i], k)
+		i++
+	}
+}
+
+func TestKeyIter_error(t *testing.T) {
+	mc := mocks.NewAPIMock(t)
+	client := Client{mc}
+
+	it := client.ListAllServiceKeys(nil, nil)
+
+	contToken := "abcdef"
+	keys := []types.GetServiceKeyResponse{
+		{Name: "a"},
+		{Name: "b"},
+		{Name: "c"},
+		{Name: "d"},
+	}
+
+	mc.EXPECT().ListServiceKeys(mock.Anything, (*string)(nil), (*int32)(nil), (*types.KeyStates)(nil)).
+		Return(&types.ListServiceKeysResponse{IsTruncated: true, ContinuationToken: &contToken, ObjectsList: keys[:2]}, nil).
+		Once()
+
+	mc.EXPECT().ListServiceKeys(mock.Anything, &contToken, (*int32)(nil), (*types.KeyStates)(nil)).
+		Return(nil, errors.New("Failure")).
+		Once()
+
+	i := 0
+	for k, err := range it.Iter(context.Background()) {
+		if i > 1 {
+			require.Error(t, err)
+		} else {
+			require.NoError(t, err)
+			require.EqualValues(t, &keys[i], k)
+		}
+		i++
+	}
+}
